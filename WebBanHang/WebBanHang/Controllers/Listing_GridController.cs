edef7a6fe8cb74e6966c6336cce735ee3a9cb3cb
Nguyễn Trường Giang <balabilu27@gmail.com>
2023-03-02 08:55:52 +0700

    first commit

---
+++ WebBanHang/WebBanHang/Controllers/Listing_GridController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebBanHang.Controllers
+{
+    public class Listing_GridController : Controller
+    {
+        // GET: Listing_Grid
+        public ActionResult Listing_Grid()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebBanHang/WebBanHang/Controllers/Listing_LargeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebBanHang.Controllers
+{
+    public class Listing_LargeController : Controller
+    {
+        // GET: Listing_Large
+        public ActionResult Listing_Large()
+        {
+            return View();
+        }
+    }
+}
