9b09d0b1429928700710303ebb4bd5f24cf12ea3
Nguyễn Trường Giang <balabilu27@gmail.com>
2023-03-20 21:37:10 +0700

    first commit

--- WebBanHang/WebBanHang/Controllers/LoginController.cs
+++ WebBanHang/WebBanHang/Controllers/LoginController.cs
@@ -1,17 +1,72 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
+using WebBanHang.Context;
 
 namespace WebBanHang.Controllers
 {
     public class LoginController : Controller
     {
         // GET: Login
+        WebsitebanhangEntities3 objModel = new WebsitebanhangEntities3();
+        //create a string MD5
+        public static string GetMD5(string str)
+        {
+            MD5 md5 = new MD5CryptoServiceProvider();
+            byte[] fromData = Encoding.UTF8.GetBytes(str);
+            byte[] targetData = md5.ComputeHash(fromData);
+            string byte2String = null;
+
+            for (int i = 0; i < targetData.Length; i++)
+            {
+                byte2String += targetData[i].ToString("x2");
+
+            }
+            return byte2String;
+        }
         public ActionResult Login()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(string email, string password)
+        {
+            if (ModelState.IsValid)
+            {
+
+
+                var f_password = GetMD5(password);
+                var data = objModel.User.Where(s => s.Email.Equals(email) && s.Password.Equals(f_password)).ToList();
+                if (data.Count() > 0)
+                {
+                    //add session
+                    Session["FullName"] = data.FirstOrDefault().FirstName + " " + data.FirstOrDefault().LastName;
+                    Session["Email"] = data.FirstOrDefault().Email;
+                    Session["idUser"] = data.FirstOrDefault().Id;
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    ViewBag.error = "Login failed";
+                    return RedirectToAction("Login");
+                }
+            }
+            return View();
+        }
+
+
+        //Logout
+        public ActionResult Logout()
+        {
+            Session.Clear();//remove session
+            return RedirectToAction("Login");
         }
     }
 }
--- WebBanHang/WebBanHang/Controllers/RegistorController.cs
+++ WebBanHang/WebBanHang/Controllers/RegistorController.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 using WebBanHang.Context;
@@ -9,15 +12,53 @@ namespace WebBanHang.Controllers
 {
     public class RegistorController : Controller
     {
+        //create a string MD5
+        public static string GetMD5(string str)
+        {
+            MD5 md5 = new MD5CryptoServiceProvider();
+            byte[] fromData = Encoding.UTF8.GetBytes(str);
+            byte[] targetData = md5.ComputeHash(fromData);
+            string byte2String = null;
+
+            for (int i = 0; i < targetData.Length; i++)
+            {
+                byte2String += targetData[i].ToString("x2");
+
+            }
+            return byte2String;
+        }
+        WebsitebanhangEntities3 objModel = new WebsitebanhangEntities3();
         // GET: Registor
+        [HttpGet]
         public ActionResult Registor()
         {
             return View();
         }
         [HttpPost]
-        public ActionResult Registor(User objuser)
+        [ValidateAntiForgeryToken]
+        public ActionResult Registor(User _user)
         {
+            if (ModelState.IsValid)
+            {
+                var check = objModel.User.FirstOrDefault(s => s.Email == _user.Email);
+                if (check == null)
+                {
+                    _user.Password = GetMD5(_user.Password);
+                    objModel.Configuration.ValidateOnSaveEnabled = false;
+                    objModel.User.Add(_user);
+                    objModel.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    ViewBag.error = "Email already exists";
+                    return View();
+                }
+
+
+            }
             return View();
         }
     }
+   
 }
